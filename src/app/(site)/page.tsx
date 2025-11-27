3480047d7eaee92ab83cf0256d6086f59b38950b
NgurahDhananjaya <99886528+NgurahDhananjaya@users.noreply.github.com>
2025-11-28 02:06:24 +0800

    New Brand Color & Button UI

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -3,7 +3,19 @@ import { BrandButton } from "@/app/components/ui/button"
 export default function HomePage() {
   return (
     <div>
-      <BrandButton></BrandButton>
+      <BrandButton variant="yellow">Contact Us</BrandButton>
+      <BrandButton variant="white">Contact Us</BrandButton>
+      <BrandButton variant="black">Contact Us</BrandButton>
+      <div className="flex flex-row">
+        <BrandButton variant="red">Contact Us</BrandButton>
+        <BrandButton variant="red" loading={true}>Contact Us</BrandButton>
+      </div>
+      <BrandButton variant="ghost">Contact Us</BrandButton>
+      <p className="font-raleway">HELLOW WORDL</p>
+      <p className="font-raleway tracking-wider">HELLOW WORDL</p>
+      <p className="w-1/6 font-raleway leading-[125%]">Menyederhanakan kebutuhan bisnis, legal, dan imigrasi secara profesional, transparan, dan berintegritas.</p>
+
+
     </div>
   );
 }
--- src/app/components/ui/button.tsx
+++ src/app/components/ui/button.tsx
@@ -2,21 +2,35 @@ import type { ButtonHTMLAttributes, PropsWithChildren } from 'react';
 import { cn } from '@/app/lib/cn';
 
 type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
-  variant?: 'primary' | 'secondary' | 'ghost';
+  variant?: 'yellow' | 'white' | 'black' | 'red' | 'ghost';
+  loading?: boolean;
 };
 
-export function BrandButton({ children, className, variant = 'primary', ...props }: PropsWithChildren<ButtonProps>) {
+export function BrandButton({ 
+  children, 
+  className, 
+  variant = 'yellow', 
+  loading = false,
+  type = 'button',
+  ...props }: PropsWithChildren<ButtonProps>) {
+    
   return (
     <button
+      type={type}
       className={cn(
-        'inline-flex items-center justify-center rounded-md px-4 py-2 text-sm font-medium transition',
-        variant === 'primary' && 'bg-brand-700 text-white hover:bg-brand-500',
-        variant === 'secondary' && 'border border-brand-700 text-brand-700 hover:bg-brand-50',
-        variant === 'ghost' && 'text-brand-700 hover:bg-brand-50',
+        'inline-flex items-center h-fit px-7 py-2.5 border rounded-xl text-black font-raleway font-light text-base tracking-wider leading-[125%] cursor-pointer transition-colors gap-2.5 focus-visible:outline-none focus-visible:rounded-xl focus-visible:ring-2 focus-visible:ring-brand-maroon focus-visible:ring-offset-2',
+        variant === 'yellow' && 'border-brand-yellow bg-brand-yellow hover:bg-brand-yellow-semi font-semibold text-white',
+        variant === 'white' && 'border-brand-black bg-brand-white hover:bg-brand-white-semi text-brand-black',
+        variant === 'black' && 'border-brand-white bg-brand-black hover:bg-brand-black-semi text-brand-white',
+        variant === 'red' && 'border-brand-maroon bg-brand-maroon hover:bg-brand-burgundy text-brand-white ',
+        variant === 'ghost' && 'border-none bg-transparent rounded-none hover:bg-brand-black/5',
         className,
       )}
       {...props}
     >
+      {loading && (
+        <span className="size-[1em] aspect-square animate-spin rounded-full border-3 border-white border-b-transparent" />
+      )}
       {children}
     </button>
   );
